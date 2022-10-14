1ad1299608df2d7cd02fae514b3189b75af7aa0f
Дмитрий <res87-87@mail.ru>
2022-10-14 23:22:38 +0300

    добавялет вторую часть задания и вариант с неверными аргументами

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 function randomNumber (min, max) {
   let randomResult;
   if (min >= 0 && max >= 0) {
-    randomResult = Math.random() * (max - min + 1);
+    randomResult = Math.random() * (max - min + 1) + min;
+  } else {
+    return NaN;
   }
   return Math.round(randomResult);
 }
 
 randomNumber(1, 5);
+
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
+
+checkStringLength(2, 20);
